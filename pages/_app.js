bb3fa64bc58b1c723cbd545d45e173aba2b700bc
Robert Driskell <driskell.ba@gmail.com>
2020-08-08 14:19:12 -0500

    added images, removed antd

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "../styles/globals.css";
+// necessary for antd styles to work properly. This is a little goofy.
+import "../styles/theme.less";
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
